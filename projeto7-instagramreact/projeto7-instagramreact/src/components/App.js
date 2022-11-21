181ca4d89f66f527578de11881d8f077afa65268
jmarcossss <joaomalcantaraaa@gmail.com>
2022-11-21 01:27:14 -0300

    Navbar(topo) ok

---
+++ projeto7-instagramreact/projeto7-instagramreact/src/components/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar"
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+            {/* <div class="corpo">
+
+            </div> */}
+        </div>
+    )
+}
