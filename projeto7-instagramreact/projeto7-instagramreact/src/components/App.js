6303bf8c24e713b888b3d00b339121770f3a95e9
jmarcossss <joaomalcantaraaa@gmail.com>
2022-11-21 02:21:02 -0300

    layout ready

--- projeto7-instagramreact/projeto7-instagramreact/src/components/App.js
+++ projeto7-instagramreact/projeto7-instagramreact/src/components/App.js
@@ -1,12 +1,15 @@
 import Navbar from "./Navbar"
+import Left from "./Left"
+import Sidebar from "./Sidebar"
 
 export default function App() {
     return (
         <div>
             <Navbar />
-            {/* <div class="corpo">
-
-            </div> */}
+            <div class="corpo">
+                <Left />
+                <Sidebar />
+            </div>
         </div>
     )
 }
---
+++ projeto7-instagramreact/projeto7-instagramreact/src/components/Sidebar.js
@@ -0,0 +1,92 @@
+export default function Sidebar() {
+    return (
+        <div class="sidebar">
+        <div class="usuario">
+          <img src="assets/catanacomics.png" />
+          <div class="texto">
+            <strong>catanacomics</strong>
+            <span>
+              Catana
+              <ion-icon name="pencil"></ion-icon>
+            </span>
+          </div>
+        </div>
+
+        <div class="sugestoes">
+          <div class="titulo">
+            Sugestões para você
+            <div>Ver tudo</div>
+          </div>
+
+          <div class="sugestao">
+            <div class="usuario">
+              <img src="assets/badvibesmemes.png" />
+              <div class="texto">
+                <div class="nome">bad.vibes.memes</div>
+                <div class="razao">Segue você</div>
+              </div>
+            </div>
+
+            <div class="seguir">Seguir</div>
+          </div>
+
+          <div class="sugestao">
+            <div class="usuario">
+              <img src="assets/chibirdart.png" />
+              <div class="texto">
+                <div class="nome">chibirdart</div>
+                <div class="razao">Segue você</div>
+              </div>
+            </div>
+
+            <div class="seguir">Seguir</div>
+          </div>
+
+          <div class="sugestao">
+            <div class="usuario">
+              <img src="assets/razoesparaacreditar.png" />
+              <div class="texto">
+                <div class="nome">razoesparaacreditar</div>
+                <div class="razao">Novo no Instagram</div>
+              </div>
+            </div>
+
+            <div class="seguir">Seguir</div>
+          </div>
+
+          <div class="sugestao">
+            <div class="usuario">
+              <img src="assets/adorableanimals.png" />
+              <div class="texto">
+                <div class="nome">adorable_animals</div>
+                <div class="razao">Segue você</div>
+              </div>
+            </div>
+
+            <div class="seguir">Seguir</div>
+          </div>
+
+          <div class="sugestao">
+            <div class="usuario">
+              <img src="assets/smallcutecats.png" />
+              <div class="texto">
+                <div class="nome">smallcutecats</div>
+                <div class="razao">Segue você</div>
+              </div>
+            </div>
+
+            <div class="seguir">Seguir</div>
+          </div>
+        </div>
+
+        <div class="links">
+          Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes •
+          Hashtags • Idioma
+        </div>
+
+        <div class="copyright">
+          © 2021 INSTAGRAM DO FACEBOOK
+        </div>
+      </div>
+    )
+}
